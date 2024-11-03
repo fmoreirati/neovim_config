ed5f3a9d1a06f366ba57ce0bec672ec7e5baec7a
fmoreira <fabianomoreira.ti@gmail.com>
2024-11-03 15:31:15 -0300

    updates

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,6 +1,22 @@
+local dap = require("dap")
+
+dap.configurations = {
+  html = {
+    name = "Launch Chrome",
+    type = "pwa-chrome",
+    request = "launch",
+    url = "http://localhost:5500",
+    webRoot = "${workspaceFolder}",
+    sourceMaps = true,
+  },
+}
+
 return {
   "mxsdev/nvim-dap-vscode-js",
   requires = { "mfussenegger/nvim-dap" },
 
   require("dap.ext.vscode").load_launchjs(nil, {}),
+
+  vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" }),
+  vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" }),
 }
--- lua/plugins/eslint.lua
+++ lua/plugins/eslint.lua
@@ -1,7 +1,22 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = { eslint = {} },
+    servers = {
+      eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectories = { mode = "auto" },
+        },
+      },
+      volar = {
+        init_options = {
+          vue = {
+            hybridMode = true,
+          },
+        },
+      },
+      vtsls = {},
+    },
     setup = {
       eslint = function()
         require("lazyvim.util").lsp.on_attach(function(client)
@@ -13,5 +28,17 @@ return {
         end)
       end,
     },
+    function(_, opts)
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
   },
 }
