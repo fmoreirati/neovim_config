5bc8e521a644d6ca283e27466d95fc880070abd1
fMoreira <fabianomoreira.ti@gmail.com>
2025-04-29 23:37:00 -0300

    updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+local augroup = vim.api.nvim_create_augroup("lsp_blade_workaround", { clear = true })
+
+-- Autocommand to temporarily change 'blade' filetype to 'php' when opening for LSP server activation
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup,
+  pattern = "*.blade.php",
+  callback = function()
+    vim.bo.filetype = "php"
+  end,
+})
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,22 +1,24 @@
 return {
-  "mfussenegger/nvim-dap",
-  optional = true,
-  opts = function()
-    local dap = require("dap")
-    local path = require("mason-registry").get_package("php-debug-adapter"):get_install_path()
-    --PHP
-    dap.adapters.php = {
-      type = "executable",
-      command = "node",
-      args = { path .. "/extension/out/phpDebug.js" },
-    }
-    --Java
-    dap.configurations.java = {
-      type = "java",
-      request = "attach",
-      name = "Debug (Attach) - Remote",
-      hostName = "127.0.0.1",
-      port = 5005,
-    }
-  end,
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      local path = require("mason-registry").get_package("php-debug-adapter"):get_install_path()
+      --PHP
+      dap.adapters.php = {
+        type = "executable",
+        command = "node",
+        args = { path .. "/extension/out/phpDebug.js" },
+      }
+      --Java
+      dap.configurations.java = {
+        type = "java",
+        request = "attach",
+        name = "Debug (Attach) - Remote",
+        hostName = "127.0.0.1",
+        port = 5005,
+      }
+    end,
+  },
 }
